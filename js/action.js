4ceabba35c5cd609f7ff07705cf0c8f6ef2a1ce2
Hassan Azzam <hassanazzam95@gmail.com>
2016-08-05 17:24:46 +0200

    Update action.js

--- js/action.js
+++ js/action.js
@@ -1,32 +1,32 @@
 
 var contact=function(mail,service,desc){
-	var title = document.querySelector('#sales *[name="service"]');
-	var desc = document.querySelector('#sales *[name="desc"]');
+	var title = document.querySelector('#sales *[name="service"]').value;
+	var desc = document.querySelector('#sales *[name="desc"]').value;
 	var body = '<p style="font-size:16px"><b>Type: </b>Contact sales<br><b>Mail: </b>'+mail+'<br><b>Service: </b>'+service+'<br><b>Description: </b>'+desc; 
 	send(body,"[locadifferent][Contact]");
 }
 
 var opinion=function(mail,title,desc){
-	var mail = document.querySelector('#opinion *[name="mail"]');
-	var title = document.querySelector('#opinion *[name="title"]');
-	var desc = document.querySelector('#opinion *[name="desc"]');
+	var mail = document.querySelector('#opinion *[name="mail"]').value;
+	var title = document.querySelector('#opinion *[name="title"]').value;
+	var desc = document.querySelector('#opinion *[name="desc"]').value;
 	var body='<p style="font-size:16px"><b>Type: </b>Opinion<br><b>Mail: </b>'+mail+'<br><b>Title: </b>'+title+'<br><b>Description: </b>'+desc;
 	send(body,"[locadifferent][Opinion]");
 }
 
 var apply=function(){
-	var desc = document.querySelector('#apply *[name="desc"]');
-	var attach = document.querySelector('#apply *[name="attach"]');
+	var desc = document.querySelector('#apply *[name="desc"]').value;
+	var attach = document.querySelector('#apply *[name="attach"]').value;
 	var body='<p style="font-size:16px"><b>Type: </b>Apply for a job<br><b>Cover letter: </b>'+desc+'<br><b>Attachment: </b><a href="'+attach+'">CV</a>';
 	send(body,"[locadifferent][Apply]");
 }
 
 var download=function(){
-	var name = document.querySelector('#pricelist *[name="name"]');
-	var mail = document.querySelector('#pricelist *[name="mail"]');
-	var comp = document.querySelector('#pricelist *[name="comp"]');
-	var title = document.querySelector('#pricelist *[name="title"]');
-	var desc = document.querySelector('#pricelist *[name="desc"]');
+	var name = document.querySelector('#pricelist *[name="name"]').value;
+	var mail = document.querySelector('#pricelist *[name="mail"]').value;
+	var comp = document.querySelector('#pricelist *[name="comp"]').value;
+	var title = document.querySelector('#pricelist *[name="title"]').value;
+	var desc = document.querySelector('#pricelist *[name="desc"]').value;
 	var body='<p style="font-size:16px"><b>Type: </b>Download prices list<br><b>Name: </b>'+name+'<br><b>Mail: </b>'+mail+'<br><b>Company Name: </b>'+comp+'<br><b>Job Title: </b>'+title+'<br><b>Description: </b>'+desc;
 	send(body,"[locadifferent][Download Prices List]");
 }
